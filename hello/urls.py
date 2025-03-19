60aeeb6311b3345290e4b227bfdda02646639ca0
Syed Ahmed Basha <syedahmedd1905@gmail.com>
2025-03-19 05:37:46 +0530

    Updated Django project with new features/fixes

--- hello/urls.py
+++ hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "JB FITNESS Admin"
+admin.site.site_title = "JB FITNESS Admin Portal"
+admin.site.index_title = "Welcome to JB FITNESS"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("home.urls"))
