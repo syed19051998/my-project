688d9c0680e332be4c303f94baf8bedf38f44b7c
Syed Ahmed Basha <syedahmedd1905@gmail.com>
2025-03-23 15:53:55 +0530

    Fixed __str__ method in Contact model & updated views

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=13)
+    desc = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} - {self.phone} - {self.date}"
--- home/views.py
+++ home/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
+from django.contrib import messages
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 def about(request):
-    return HttpResponse("This is About page")
+    return render(request,"about.html")
 
 def services(request):
-    return HttpResponse("This is services page")
+    return render(request,"services.html")
 
 def contact(request):
-    return HttpResponse("This is contact page")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email,phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, "Your Request is submitted!")
+
+    return render(request,"contact.html")
